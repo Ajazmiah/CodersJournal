745bbaeefd3029c451c736452c2864bbdf1639d4
Ajaz Miah <ajazmiah@ajazs-air.lan>
2024-06-10 03:17:12 -0400

    fixing header

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -7,6 +7,7 @@ import classNames from "classnames";
 import heroImage from "../../../public/heroImage.jpg";
 import { Link } from "react-router-dom";
 import Image from "../Image/Image";
+import RegisterForm from "../RegisterForm/RegisterForm";
 
 const Hero = () => {
   const { userInfo } = useSelector((state) => state.auth);
@@ -15,29 +16,20 @@ const Hero = () => {
   if (userInfo) return;
 
   const content = (
-    <>
-      <div>
-        <div>
-          <div className={Styles.phraseContent}>
-            <div className={classNames(Styles.phrase, "space-bottom-1")}>
-              <p className={Styles.phrasePrimary}>Craft.Connect.Captivate</p>
-              <p className={Styles.phraseSecondary}>
-                Write To The World & Inspire with Inkspire
-              </p>
-            </div>
-            <Link to="/signup" className={Styles.getStarted}>
-              Get Started
-            </Link>
-          </div>
-        </div>
+   <div>
+     <div className={Styles.leftSide}>
+      <p>
+        Where Your Stories Come to Life
+        <span>Write, Share, Inspire.</span>
+      </p>
+      <button>Start Writing</button>
+     </div>
 
-       <div  className={Styles.item}>
-          <Container>
-            <Image/>
-           </Container>
-        </div>
-      </div>
-    </>
+     <div className={Styles.rightSide}>
+      <RegisterForm/>
+
+     </div>
+   </div>
   );
   return <div className={Styles.hero}>{content}</div>;
 };
