75c1f7a0821d9a1756ba386227503fd91444f65c
Ajaz Miah <ajazmiah@ajazs-air.lan>
2024-06-11 01:13:09 -0400

    clean up header

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -7,9 +7,7 @@ import { useNavigate } from "react-router-dom";
 import Styles from "./Header.module.css";
 import DropDownMenu from "../DropDownMenu/DropDownmenu.jsx";
 import ProfileImage from "../ProfileImage/ProfileImage.jsx";
-
 import Logo from "../Logo/Logo.jsx";
-
 import { backdropContext } from "../../context/backdropContext.jsx";
 import useNavigationItem from "../../hooks/useNavigationItem.jsx";
 import VerticalModal from "../VerticalModal/verticalModal.jsx";
@@ -19,14 +17,11 @@ import Backdrop from "../Backdrop/Backdrop.jsx";
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
-
   const [logoutApiCall] = useLogoutMutation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const [userSettingMenu, pagesNavigation, userInfo] = useNavigationItem();
   const [isBackdropOpen, setOpenBackdrop] = useContext(backdropContext);
-
   const [openNav, setOpenNav] = useState(false);
 
   const logoutHandler = async () => {
@@ -71,7 +66,10 @@ function ResponsiveAppBar() {
 
             {openNav ? (
               <Backdrop>
-                <VerticalModal classes={Styles} pagesNavigation={pagesNavigation} />
+                <VerticalModal
+                  classes={Styles}
+                  pagesNavigation={pagesNavigation}
+                />
               </Backdrop>
             ) : null}
           </div>
@@ -83,7 +81,11 @@ function ResponsiveAppBar() {
           <nav className={Styles.nav}>
             {pagesNavigation.map((page) => (
               <div key={page.to} className={Styles.navItem}>
-                <Link to={page.to} className={Styles.navLink} onClick={handleCloseNavMenu}>
+                <Link
+                  to={page.to}
+                  className={Styles.navLink}
+                  onClick={handleCloseNavMenu}
+                >
                   {page.text}
                 </Link>
               </div>
@@ -93,19 +95,34 @@ function ResponsiveAppBar() {
           {/* USER SETTING MENU */}
           {userInfo ? (
             <div className={Styles.userMenu}>
-              <button className={Styles.profileButton} onClick={handleOpenUserMenu}>
+              <button
+                className={Styles.profileButton}
+                onClick={handleOpenUserMenu}
+              >
                 <ProfileImage
                   customClasses="headerImage"
                   imageURL={userInfo?.profilePicture || false}
                 />
               </button>
-              {anchorElUser && (
-                <div className={Styles.menu} id="menu-appbar">
-                  {userInfo?._id && (
-                    <DropDownMenu dropDownItems={userSettingMenu} handleClick={logoutHandler} />
-                  )}
-                </div>
-              )}
+              <div className={Styles.menu} id="menu-appbar">
+                <ul>
+                  {userInfo?._id &&
+                    userSettingMenu.map(({ Element, text, to }) => (
+                      <li className={Styles.listItem} key={text}>
+                        {Element ? (
+                          <button
+                            sx={{ margin: 0, padding: 0, color: "#ffff" }}
+                            onClick={logoutHandler}
+                          >
+                            {text}
+                          </button>
+                        ) : (
+                          <Link to={to}>{text}</Link>
+                        )}
+                      </li>
+                    ))}
+                </ul>
+              </div>
             </div>
           ) : null}
         </div>
