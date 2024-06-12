6167b955f5e94a0a96d5f09c13f6efd8f343f400
Ajaz Miah <ajazmiah@ajazs-air.lan>
2024-06-12 00:50:30 -0400

    nav dropdown

--- frontend/src/components/DropDownMenu/DropDownmenu.jsx
+++ frontend/src/components/DropDownMenu/DropDownmenu.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Styles from "./DropDownMenu.module.css";
+import { FaSignOutAlt } from "react-icons/fa";
+import { icons, getIcon } from "../Icon/index.jsx";
 import { Button } from "@mui/material";
 
 function DropDownMenu({ dropDownItems, handleClick }) {
-  return dropDownItems.map(({ Element, text, to }) =>
-    Element ? (
-      <li className={Styles.listItem}>
-        <Button sx={{ margin: 0, padding: 0, color: '#ffff' }} key={text} onClick={handleClick}>
-          {text}
-        </Button>
-      </li>
-    ) : (
-      <li className={Styles.listItem}>
-        <Link to={to} key={to}>
-          {text}
-        </Link>
-      </li>
-    )
-  );
+  return dropDownItems.map(({ Element, text, to }) => (
+    <li className={classNames(Styles.listItem, "flex")} key={text}>
+      {Element ? (
+        <>
+          <button
+            sx={{ margin: 0, padding: 0, color: "#ffff" }}
+            onClick={logoutHandler}
+          >
+            {text}
+          </button>
+          <FaSignOutAlt />
+        </>
+      ) : (
+        <>
+          <Link to={to}>{text}</Link>
+          {getIcon(text.split(" ").join(""))}
+        </>
+      )}
+    </li>
+  ));
 }
 
 export default DropDownMenu;
