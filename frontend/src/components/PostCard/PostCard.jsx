d5690a5c4d666d0fbb2bd9b079608d80ddc7feff
Ajaz Miah <ajazmiah@ajazs-air.lan>
2024-08-23 00:51:31 -0400

    clean up

--- frontend/src/components/PostCard/PostCard.jsx
+++ frontend/src/components/PostCard/PostCard.jsx
@@ -21,22 +21,11 @@ function PostCard({ posts }) {
             className={Styles.post}
             onClick={() => handleNavigateToPost(post)}
           >
-            {'haha'+ post?.coverImageName}
             <div className={classNames("space-bottom", Styles.postCoverImage)}>
               <img
                 width={"100%"}
                 height={"200px"}
-                src={
-                  post?.coverImage ||
-                  "https://images.pexels.com/photos/159618/still-life-school-retro-ink-159618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                }
-              />
-               <img
-                width={"100%"}
-                height={"200px"}
-                src={
-                  post?.coverImageName
-                }
+                src={post?.coverImageName || post?.coverImage}
               />
             </div>
 
