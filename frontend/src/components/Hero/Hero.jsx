5e7c7a6d950673f4b16aedfe4d129ca2d1c49b66
Ajaz Miah <ajazmiah@ajazs-air.lan>
2024-08-29 00:29:50 -0400

    style

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -8,25 +8,32 @@ import { Link } from "react-router-dom";
 import Image from "../Image/Image";
 import Button from "../Atoms/Button/Button";
 
-
 const Hero = () => {
   const { userInfo } = useSelector((state) => state.auth);
-
 
   if (userInfo) return;
 
   const content = (
-   <div className={Styles.hero}>
-     <div className={'container'}>
-      <p className={Styles.tagLines}>
-        <span className={Styles.mainTagLine}> Spread The Knowledge of Coding</span>
-        <span className={Styles.subTagLine}>Learn. Share. Inspire.</span>
-      </p>
+    
+      <div className={"container"}>
+        <p className={Styles.tagLines}>
+          <span className={Styles.mainTagLine}>
+            {" "}
+            Spread The Knowledge of Coding
+          </span>
+          <span className={Styles.subTagLine}>Learn. Share. Inspire.</span>
+        </p>
 
-      <Button classes={classNames(Styles.btn, Styles.btnNonTransparent)}>Read</Button>
-      <Button lasses={classNames(Styles.btn, Styles.btnTransparent)}>Start Writing</Button>
-     </div>
-   </div>
+        <div className={Styles.btns}>
+          <button className={classNames(Styles.btn, Styles.btnNonTransparent)}>
+            Read
+          </button>
+          <button className={classNames(Styles.btn, Styles.btnTransparent)}>
+            Start Writing
+          </button>
+        </div>
+      </div>
+
   );
   return <div className={Styles.hero}>{content}</div>;
 };
