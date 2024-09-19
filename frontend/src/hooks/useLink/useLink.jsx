a343f8f55ab50e7bad9dee573095f456d29004a2
Ajaz Miah <ajazmiah@ajazs-air.lan>
2024-09-18 20:10:22 -0400

    fixed

--- frontend/src/hooks/useLink/useLink.jsx
+++ frontend/src/hooks/useLink/useLink.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import {useNavigate, useLocation  } from "react-router-dom";
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export function useLink() {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const baseUrl = location.pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const baseUrl = location.pathname;
 
-    const handleNavigateToPost = (post, path=null) => {
-        navigate(baseUrl.replace(baseUrl, path ? path : `/post/${post._id}`));
-      };
-  return {handleNavigateToPost}
+  const handleNavigateToPost = (post, path = null) => {
+    navigate(baseUrl.replace(baseUrl, path ? path : `/post/${post._id}`));
+  };
+  return { handleNavigateToPost };
 }
-
