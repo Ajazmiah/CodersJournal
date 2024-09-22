1d10b52828f065fd62e3151db798e94b7d77af28
abdul <abdulkhaliq94@gmail.com>
2024-09-21 21:05:25 -0400

    updated with toggle color mode component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,25 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Sidebar from "./components/SideBar/SideBar";
 import Footer from "./components/Footer/Footer";
+import ToggleColorMode from "./components/ToggleColorMode/ToggleColorMode";
+import toggleStyle from "./components/ToggleColorMode/ToggleColorMode.module.css";
+import CssBaseline from '@mui/material/CssBaseline';
+import { colors } from "@mui/material";
+
 
 function App() {
   return (
     <>
-      <Header />
-      <ToastContainer />
+      <ToggleColorMode>
+        <CssBaseline />
+        <Header />
+        <ToastContainer />
 
-      <main className="main">
-        <Outlet />
-      </main>
-      <Footer/>
+        <main className="main">
+          <Outlet />
+        </main>
+        <Footer />
+      </ToggleColorMode>
     </>
   );
 }
