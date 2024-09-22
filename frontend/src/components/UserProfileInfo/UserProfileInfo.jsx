6f2c3c9ed4c7420408259af6584a93a65762ada7
abdul <abdulkhaliq94@gmail.com>
2024-09-21 21:03:33 -0400

    made update account to private logged in user access only

--- frontend/src/components/UserProfileInfo/UserProfileInfo.jsx
+++ frontend/src/components/UserProfileInfo/UserProfileInfo.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from 'react-redux';
 import AuthorBio from "../AuthorBio/AuthorBio";
 import { Link } from "react-router-dom";
 import Styles from "./UserProfileInfo.module.css";
@@ -6,13 +7,17 @@ import { getIcon } from "../Icon";
 import Button from "../Atoms/Button/Button";
 import classNames from "classnames";
 
-function UserProfileInfo({ userInfo }) {
+function UserProfileInfo(props) {
+
+  const {userInfo} = useSelector((state)=>state.auth)
+  console.log("user info",userInfo)
+  console.log("user props info",props.userInfo)
   return (
     <div className={Styles.userProfileInfo}>
       <div className={classNames('container', Styles.profileInfo)}>
         <div className={Styles.profileLeft}>
           <div className={Styles.profilePicture}>
-            <img src={userInfo.profilePicture} alt="" />
+            <img src={props.userInfo.profilePicture} alt="" />
           </div>
 
           <div>
@@ -22,14 +27,14 @@ function UserProfileInfo({ userInfo }) {
               <li>{getIcon("github")}</li>
               <li>{getIcon("twitter")}</li>
             </ul>
-            <Link to="/profile/update">
+            {props.userInfo._id==userInfo._id ? <Link to="/profile/update">
               <Button classes="update">Update Account</Button>
-            </Link>
+            </Link>:null}
           </div>
         </div>
 
         <div className={Styles.profileRight}>
-          <AuthorBio authorInfo={userInfo} />
+          <AuthorBio authorInfo={props.userInfo} />
         </div>
       </div>
     </div>
