6d11ae8f8111ee6a5a2e2c1aa86b7c4dbdc9475a
Ajaz Miah <ajazmiah@ajazs-air.lan>
2024-10-16 13:47:14 -0400

    /

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -192,7 +192,7 @@ const getSiteMaps = asyncHandler(async (req, res, next) => {
   const HOST =
     process.env.NODE_ENV == "development"
       ? "http://localhost:3000/"
-      : "https://coderjournal-frontend.onrender.com";
+      : "https://coderjournal-frontend.onrender.com/";
   const sitemap = new SitemapStream({ hostname: HOST });
 
   console.log("____SITE____", process.env.PORT);
