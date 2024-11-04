9b83f3ae6b748283c3c6d7e29216bc9492ffd149
Ajaz Miah <ajazmiah@ajazs-air.lan>
2024-11-04 17:15:20 -0500

    added slice

--- frontend/src/slices/postsApiSlice.js
+++ frontend/src/slices/postsApiSlice.js
@@ -41,8 +41,8 @@ const postsApi = apiSlice.injectEndpoints({
       }),
     }),
     getUserPosts: builder.mutation({
-      query: () => ({
-        url: `${POSTS_URL}/allPost`,
+      query: (limit) => ({
+        url: `${POSTS_URL}/allPost?limit=${limit}`,
         method: "GET",
       }),
     }),
@@ -55,11 +55,17 @@ const postsApi = apiSlice.injectEndpoints({
     }),
 
     expludeUserPosts: builder.mutation({
-      query: () => ({
-        url: `${POSTS_URL}/getUserPosts`,
+      query: (limit) => ({
+        url: `${POSTS_URL}/getUserPosts?limit=${limit}`,
         method: "GET",
       }),
     }),
+    sitemap: builder.mutation({
+      query:() => ({
+        url: `${POSTS_URL}/sitemap.xml`,
+        method: 'GET'
+      })
+    })
   }),
 });
 
@@ -71,4 +77,5 @@ export const {
   useEditPostMutation,
   useGetUserPostsMutation,
   useExpludeUserPostsMutation,
+  
 } = postsApi;
